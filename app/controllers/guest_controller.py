0c8638a7decf885c063e12a7e303431ab6f75d6a
Joseph Cobhams <tech.supreme1@gmail.com>
2018-10-24 10:41:54 +0100

    Generic message for guest arrival

--- app/controllers/guest_controller.py
+++ app/controllers/guest_controller.py
@@ -85,7 +85,7 @@ class GuestController(BaseController):
 				guest.tag_no = tag_no
 				
 				if beep.lower() == 'true':
-					msg = 'Psst! your guest ({}) is at the 4th floor reception.'.format(guest.guest_name)
+					msg = 'Psst! your guest ({}) has arrived the visitors waiting area.'.format(guest.guest_name)
 					self.slackhelper.post_message(msg=msg, recipient=guest.host_slackid)
 				
 			if field == 'time-out':
